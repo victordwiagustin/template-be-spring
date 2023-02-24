edf09411489213075eef3fc4dde5ce9197b8b858
victordwiagustin <victor.agustin@m2.formulatrix.com>
2023-02-24 14:09:36 +0700

    add tailwind

--- src/main/resources/static/web-app-dev/src/App.tsx
+++ src/main/resources/static/web-app-dev/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.scss";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return <div className="text-3xl font-bold underline">Hello World</div>;
 }
 
 export default App;
