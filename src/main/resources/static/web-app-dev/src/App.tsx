653d366bfb015b39539ba428796ef5de0c04093e
victordwiagustin <victor.agustin@m2.formulatrix.com>
2023-02-24 13:50:17 +0700

    add ui project with vite template

---
+++ src/main/resources/static/web-app-dev/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return <div className="App">Hello World</div>;
+}
+
+export default App;
+
+/*
+TODO:
+- recoil state management https://recoiljs.org/
+- routing
+- login
+- register
+- component management (form, input, button, layout, etc)
+- react bootstrap / material / etc OR custom 
+- SASS / TAILWIND ?? (tailwind atleast bisa buat nambah ilmu)
+*/
